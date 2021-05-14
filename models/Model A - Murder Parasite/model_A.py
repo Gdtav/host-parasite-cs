32c137a339cc0cabb3a2b1f58c6ea39d1d747e24
António Eloi <aeloi@student.dei.uc.pt>
2021-05-15 00:13:21 +0100

    Fix model A

--- models/Model A - Murder Parasite/model_A.py
+++ models/Model A - Murder Parasite/model_A.py
@@ -3,12 +3,12 @@ from pylab import *
 
 # This parasites when leave the body die and he infectes another
 
-width = 5  # Size of horizontal length
-height = 5  # Size of vertical length
+width = 3  # Size of horizontal length
+height = 3  # Size of vertical length
 hostProb = 0.0  # Probability of the cell being occupied by a healthy host
 infectedProb = 0.0  # Probability of cell being occupied by a host with parasite
 
-infectionRate = 0.15  # Probability of getting infected with an parasite
+infectionRate = 0.0  # Probability of getting infected with an parasite
 regrowthRate = 0.20 # Probability of regrowing in cellular
 deathProb = 0.0  # Probability of dying after contracting the parasite
 cureProb = 0.0 # Probability of the parasite die
@@ -73,13 +73,20 @@ def update():
             state = config[x, y]
             neigh_list = neighbours(x, y)
             shuffle(neigh_list)
-            if state == 0:  # empty
+            if state == 0 and nextConfig[x, y] == 0:  # empty
+                print(f'Checking [{x} {y}]: that is empty')
                 if random() < regrowthRate:
+                    print(f'[{x} {y}]: Regrowth healthy host')
                     state = 1
                 else:
+                    print(f'[{x} {y}]: Does not regrowth healthy host')
                     state = 0
 
+            elif state == 0 and nextConfig[x, y] == 1:  # empty
+                state = 1
+
             elif state == 1:  # healthy
+                print(f'Checking [{x} {y}]: that is healthy')
                 for neighbour in neigh_list:
                     if config[neighbour[0], neighbour[1]] == 2:  # if neighbour infected
                         if random() < infectionRate: # host becomes infected
@@ -88,11 +95,14 @@ def update():
                         break
                 else:
                     for neighbour in neigh_list:
-                        if nextConfig[neighbour[0], neighbour[1]] == 0:
+                        if config[neighbour[0], neighbour[1]] == 0 and nextConfig[neighbour[0], neighbour[1]] == 0:
                             nextConfig[neighbour[0], neighbour[1]] = 1  # healthy moves to a empty cell
                             state = 0  # will move so this cell will be empty
+                            print(f'[{x} {y}]: will be empty as healthy host moved')
+                            print(f'\t[{neighbour[0]} {neighbour[1]}]: Will be healthy host that just moved')
                             break
                     else:
+                        print(f'[{x} {y}]: Will remain a healthy host that did not moved')
                         state = 1  # does not move
 
             elif state == 2:
@@ -111,9 +121,10 @@ def update():
                         state = 2  # infected does not move
 
             nextConfig[x, y] = state
+            print(nextConfig)
 
     config[:] = nextConfig
-    nextConfig[:] = zeros([width, height])
+    nextConfig[:] = config[:]
 
     # Change state of stored values
     ## Empty - State 0
@@ -150,7 +161,6 @@ def neighbourhood(type):
             neigh_list.append([(x) % width, (y + 1) % height])
             neigh_list.append([(x) % width, (y - 1) % height])
 
-        print()
         return neigh_list
 
     return neighbourhood
